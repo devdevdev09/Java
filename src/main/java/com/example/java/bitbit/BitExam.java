3819cc49c0d19d0d3f8c1971c0059f3b9a07f87e
dev09 <devdevdevdevdev09@gmail.com>
2020-09-15 16:58:40 +0900

    add bit calc

---
+++ src/main/java/com/example/java/bitbit/BitExam.java
@@ -0,0 +1,11 @@
+package com.example.java.bitbit;
+
+public class BitExam {
+    public static void main(String[] args) {
+        int n1 = 4;
+        int n2 = 5;
+
+        System.out.println("bit calc : " + (4 | 5));
+        System.out.println("bit calc : " + Integer.toBinaryString(n1 | n2));
+    }
+}
